1887806fd1afe2021eb50542a457e5c509fd9406
Evgenii <sergeevjane@mail.ru>
2023-05-30 00:06:44 +0300

    added SoftAssertionsSearch

---
+++ src/test/java/github/SoftAssertionsSearch.java
@@ -0,0 +1,36 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsSearch {
+
+
+    @BeforeAll
+    static void beforeAll () {
+        Configuration.browser = "firefox";
+        Configuration.browserSize = "1920x1080";
+
+    }
+
+    @Test
+    void searchExampleCodeJunit5 () {
+// Открыть страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+// Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+// Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
+// Открыть страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
+
